7363bf52d43853b4387cb791794cb193a7793d32
kashimia <casimia0314@gmail.com>
2020-11-01 15:07:32 +0000

    update migrate

--- db/migrate/20201101085732_add_user_to_tasks.rb
+++ db/migrate/20201101085732_add_user_to_tasks.rb
@@ -2,7 +2,7 @@ class AddUserToTasks < ActiveRecord::Migration[5.2]
   def change
     add_reference :tasks, :user, foreign_key: true
     
-    string :content
+    string :content ,:status
     
     timestamps
   end
