f070b1ce1aa439b334ecd185df6b3ba2d6a2473d
kashimia <casimia0314@gmail.com>
2020-11-04 06:42:15 +0000

    Create Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,10 +4,8 @@ class TasksController < ApplicationController
    before_action :correct_user, only: [:show, :edit, :update, :destroy]
    
    def index
-      if logged_in?
         @task = current_user.tasks.build  # form_with 用
         @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-      end
    end
 
    def show
